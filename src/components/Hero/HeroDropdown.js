a4b254f9365b73a526c6dd81f8bb9c16b442b9e1
Emilis <emiliskleinas2003@gmail.com>
2022-11-19 14:14:53 +0200

    Initialize project using Create React App

---
+++ src/components/Hero/HeroDropdown.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-solid-svg-icons";
+
+const HeroDropdown = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon="fa-sharp fa-solid fa-square-caret-down" />
+    </div>
+  );
+};
+
+export default HeroDropdown;
